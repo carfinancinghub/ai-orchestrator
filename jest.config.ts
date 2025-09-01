bb43ea0e9e6fa2df0feb6ef8eb07d897a921401a
Car Haul <carfinancinghub@yahoo.com>
2025-08-31 18:20:05 -0700

    chore(ts-migration): add TS/TSX counterparts (no functional change)
    Auto-created from JS/JSX with // @ts-nocheck; excluded tests/mocks and build dirs
    
    Filename digit policy applied; duplicates flagged by name & size

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+﻿// @ts-nocheck
+// AUTO-CREATED from: jest.config.js on 2025-08-31T18:12:03
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
