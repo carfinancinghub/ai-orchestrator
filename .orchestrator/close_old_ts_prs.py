48809cdcb6242bc51a0657ff3439dfc3df03665f
Car Haul <carfinancinghub@yahoo.com>
2025-09-14 20:08:06 -0700

    Cod1: steps A–D helpers, first 25-file append, gates comment, report 20250914_2008

---
+++ .orchestrator/close_old_ts_prs.py
@@ -0,0 +1,9 @@
+﻿from github import Github, Auth
+import os, re
+gh   = Github(auth=Auth.Token(os.environ["GITHUB_TOKEN"]))
+repo = gh.get_repo(os.environ.get("AIO_TARGET_REPO","carfinancinghub/cfh"))
+
+for pr in repo.get_pulls(state="open"):
+    if re.match(r"ts-migration/generated-\d{8}_\d{6}", pr.head.ref):
+        pr.edit(state="closed")
+        print("Closed", pr.number, pr.head.ref)
