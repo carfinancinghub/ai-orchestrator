408d1c10a0bb047dc223dd0761a0dd1c67368cc2
Car Haul <carfinancinghub@yahoo.com>
2025-09-15 08:14:13 -0700

    Cod1: fix Git ref creation (use refs/heads/.. for create_git_ref)

--- .orchestrator/show_head.py
+++ .orchestrator/show_head.py
@@ -2,5 +2,5 @@
 import os
 gh = Github(auth=Auth.Token(os.environ["GITHUB_TOKEN"]))
 repo = gh.get_repo(os.environ.get("AIO_TARGET_REPO","carfinancinghub/cfh"))
-pr   = repo.get_pull(int("https://github.com/carfinancinghub/cfh/pull/15".split("/")[-1]))
+pr   = repo.get_pull(int("https://github.com/carfinancinghub/cfh/pull/16".split("/")[-1]))
 print("PR #", pr.number, "head.ref =", pr.head.ref, "base =", pr.base.ref, "url =", pr.html_url)
