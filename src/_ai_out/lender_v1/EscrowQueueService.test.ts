24f9b6e3f9dd2a8826bfa7bbf79cfc08df0e068b
Car Haul <carfinancinghub@yahoo.com>
2025-10-16 03:19:07 -0700

    lender: pillar plan + TS stubs

---
+++ src/_ai_out/lender_v1/EscrowQueueService.test.ts
@@ -0,0 +1,7 @@
+// Auto-generated placeholder tests for EscrowQueueService
+import { EscrowQueueService } from "./EscrowQueueService";
+
+test("EscrowQueueService instantiation", () => {
+  const x = new EscrowQueueService();
+  expect(x).toBeTruthy();
+});
---
+++ src/_ai_out/lender_v1/EscrowQueueService.ts
@@ -0,0 +1,6 @@
+// Auto-generated stub
+export class EscrowQueueService {
+  constructor() {}
+  async connect() {}
+  async disconnect() {}
+}
